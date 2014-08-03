230cea04d0106f4dd5f1533f68a01fc2c4ec4bc5
Sara Safavi <sara@sarasafavi.com>
2014-08-03 14:56:28 -0500

    add a form for creating new recipes

--- recipeminder/recipes/urls.py
+++ recipeminder/recipes/urls.py
@@ -3,5 +3,6 @@ from recipes import views
 
 urlpatterns = patterns('',
         url(r'^$', views.recipe_list, name = 'recipelist'),
-        url(r'^(?P<recipe_id>\d+)/$', views.recipe, name='recipe')
+        url(r'^(?P<recipe_id>\d+)/$', views.recipe, name='recipe'),
+        url(r'^addrecipe/', views.addrecipe, name='addrecipe')
                         )
--- recipeminder/recipes/views.py
+++ recipeminder/recipes/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from recipes.models import Recipe
 from django.http import Http404
+from recipes.forms import RecipeForm
 
 def recipe_list(request):
     """Pull the current list of all recipes and return them.
@@ -21,3 +22,25 @@ def recipe(request, recipe_id):
             raise Http404
     context = {'recipe' : recipe}
     return render(request, 'recipe.html', context)
+
+
+def addrecipe(request):
+    """Create a form that can be used to add a new recipe.
+    Save data submitted through the form to the database as a new recipe.
+    """
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            r = Recipe()
+            r.name = data['name']
+            r.servings = data['servings']
+            r.description = data['description']
+            r.ingredients = data['ingredients']
+            r.instructions = data['instructions']
+            r.save()
+            return redirect(recipe_list)
+    else:
+        form = RecipeForm()
+    return render(request, 'addrecipe.html', { 'form': form})
