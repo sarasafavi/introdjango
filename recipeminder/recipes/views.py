d28d1a6c86bcbbbf913c0cdd9782b7d1ab051ffa
Sara Safavi <sara.safavi@rackspace.com>
2015-03-31 17:16:19 -0500

    Add view to allow getting a recipe by ID via API

--- recipeminder/recipes/views.py
+++ recipeminder/recipes/views.py
@@ -67,3 +67,19 @@ def api_recipe_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(('GET',))
+def api_recipe_detail(request, id):
+    """
+    API view that returns a recipe by ID
+    """
+
+    try:
+        recipe = Recipe.objects.get(id=id)
+    except Recipe.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = RecipeSerializer(recipe)
+        return Response(serializer.data)
