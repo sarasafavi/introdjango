3d7487238ee6f080135852c6d826eb7a1806ba42
Sara Safavi <sara.safavi@rackspace.com>
2015-03-31 17:13:00 -0500

    Add URL config for API to list all recipes

--- recipeminder/recipeminder/urls.py
+++ recipeminder/recipeminder/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-import recipes
+from recipes import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'recipeminder.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^recipes/', include('recipes.urls')),
     url(r'^admin/', include(admin.site.urls)),
+)
+# API urls
+urlpatterns += format_suffix_patterns(
+    [
+        url(r'^api/recipes/$', views.api_recipe_list, name='api-recipe-list'),
+    ]
 )
