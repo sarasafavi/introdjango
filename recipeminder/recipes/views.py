7581b20438071bc6e6376499ec9471a9ec5cfe92
Sara Safavi <sara.safavi@rackspace.com>
2015-03-31 17:14:44 -0500

    Create a view to allow POSTing new recipes via API

--- recipeminder/recipes/views.py
+++ recipeminder/recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from recipes.models import Recipe
 from django.http import Http404
 from recipes.forms import RecipeForm
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from recipes.serializers import RecipeSerializer
@@ -59,3 +60,10 @@ def api_recipe_list(request):
         recipes = Recipe.objects.all()
         serializer = RecipeSerializer(recipes, many=True)
         return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
