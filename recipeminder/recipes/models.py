72370e38aca7f33c46d7497d1b03bec62da12491
Sara Safavi <sara@sarasafavi.com>
2016-03-25 18:52:53 -0500

    add a model to hold recipes in the database

--- recipeminder/recipes/models.py
+++ recipeminder/recipes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=128)  # allow super-long names
+    created = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=True)
+    servings = models.PositiveIntegerField(null=True)
+    # vs IntegerField(), must be either 0 or a positive number.
+    # good, because recipes can't have negative servings
+    ingredients = models.TextField(null=True)
+    instructions = models.TextField(null=True)
