89a797ccab91acc109fffad3adc788aaad1bc7de
Sara Safavi <sara@sarasafavi.com>
2016-03-25 19:35:40 -0500

    add a view for single recipes selected by ID, with a placeholder template for now

--- recipeminder/recipes/urls.py
+++ recipeminder/recipes/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from recipes import views
 
 urlpatterns = [
-    url(r'^$', views.recipe_list, name='recipelist')
+    url(r'^$', views.recipe_list, name='recipelist'),
+    url(r'^(?P<recipe_id>\d+)/$', views.recipe, name='recipe')
 ]
--- recipeminder/recipes/views.py
+++ recipeminder/recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from recipes.models import Recipe
 
 
@@ -6,3 +7,12 @@ def recipe_list(request):
     recipelist = Recipe.objects.all()
     context = {'recipelist': recipelist}
     return render(request, 'recipelist.html', context)
+
+
+def recipe(request, recipe_id):
+    try:
+        recipe = Recipe.objects.get(id=recipe_id)
+    except Recipe.DoesNotExist:
+        raise Http404
+    context = {'recipe': recipe}
+    return render(request, 'recipe.html', context)
