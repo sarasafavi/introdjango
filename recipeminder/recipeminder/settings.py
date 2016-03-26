c66bba9fd5d70b0d63ac9325049c0f5e13755bc0
Sara Safavi <sara@sarasafavi.com>
2016-03-25 20:01:33 -0500

    use django-bootstrap3 to style templates

--- recipeminder/recipeminder/settings.py
+++ recipeminder/recipeminder/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+    'recipes',
+    'bootstrap3'
 ]
 
 MIDDLEWARE_CLASSES = [
