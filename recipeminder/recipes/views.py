4446984dde6bfc99353e2cb4911ba7a2fe9930ef
Sara Safavi <sara@sarasafavi.com>
2016-03-25 19:14:57 -0500

    let recipe list view pull all recipes from database

--- recipeminder/recipes/views.py
+++ recipeminder/recipes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import HttpResponse
+from recipes.models import Recipe
 
 
 def recipe_list(request):
-    return HttpResponse("This will be a list of all recipes.")
+    recipelist = Recipe.objects.all()
+    names = '<li>'.join([r.name for r in recipelist])
+    output = '<ul><li>{}</ul>'.format(names)
+    return HttpResponse(output)
