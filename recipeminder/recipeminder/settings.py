d8f39835b397aad4a87840d9197922376a4771f6
Sara Safavi <sara@sarasafavi.com>
2016-03-25 20:02:53 -0500

    use a free third-party stylesheet to make Bootstrap prettier

--- recipeminder/recipeminder/settings.py
+++ recipeminder/recipeminder/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BOOTSTRAP3 = {
+
+    'theme_url': 'https://bootswatch.com/united/bootstrap.min.css'
+}
